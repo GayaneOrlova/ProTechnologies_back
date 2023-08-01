16959613b77474386c7f652fe707702c4a42c5d0
Гаяне <gayanev.stepanyan@yandex.ru>
2023-08-01 14:49:49 +0300

    pagination_PageNumberPagination

--- config/settings.py
+++ config/settings.py
@@ -145,12 +145,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 2
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 4
     ,
-    #  'DEFAULT_PAGINATION_CLASS': 'CustomPagination',
-    # 'PAGE_SIZE': 2
-    # ,
 }
 
 
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,8 @@
 from rest_framework import permissions, viewsets
+from rest_framework.pagination import PageNumberPagination, OrderedDict
+from rest_framework.response import Response
 from projects.models import Technology, Project
 from .permissions import IsAuthorOrReadOnly
-# from services import MyPagination
-from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination, BasePagination, OrderedDict
-from rest_framework.response import Response
 from projects.serializers import (
     TechnologyReadSerializer,
     ProjectReadSerializer,
@@ -11,33 +10,16 @@ from projects.serializers import (
 )
 
 # class CustomPagination(PageNumberPagination):
-
 #     def get_paginated_response(self, data):
-#         return Response({
-#             'links': {
-#               'next': self.get_next_link(),
-#               'previous': self.get_previous_link()
-#             },
-#             'count': self.page.paginator.count,
-#             'results': data,
-#             'offset': self.
-
-#         })
-        
-#         const offset = (page - 1) * 2;
-
-
-class CustomPagination(LimitOffsetPagination):
-    def get_paginated_response(self, data):
-        return Response(OrderedDict([
-            ("links", {
-                'next': self.get_next_link(),
-                'previous': self.get_previous_link(),
-            }),
-            ('count', self.count),
-            ('results', data),
-            ('offset', self.offset)
-        ]))
+#         return Response(OrderedDict([
+#             ("links", {
+#                 'next': self.get_next_link(),
+#                 'previous': self.get_previous_link(),
+#                 # 'offset': self.offset,
+#                 # 'count': self.count
+#             }),
+#             ('results', data),
+#         ]))
  
 class TechnologyViewSet(viewsets.ReadOnlyModelViewSet):
     """
@@ -48,17 +30,13 @@ class TechnologyViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = (permissions.AllowAny,)
     
 
-class ProjectViewSet(viewsets.ModelViewSet,LimitOffsetPagination ):
+class ProjectViewSet(viewsets.ModelViewSet ):
     """
     CRUD posts
     """
     queryset = Project.objects.all()
-    pagination_class = CustomPagination
-    # pagination_class = LimitOffsetPagination
+    pagination_class = PageNumberPagination
     
-    # In order to use different serializers for different 
-    # actions, you can override the 
-    # get_serializer_class(self) method
     def get_serializer_class(self):
         if self.action in ("create", "update", "partial_update", "destroy"):
             return ProjectWriteSerializer
